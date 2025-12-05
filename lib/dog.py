43f159425a6c6c9ca7e6b7cb59bbeda7f9b63918
kymking <kimaniantidote655@gmail.com>
2025-12-05 12:24:39 +0300

    commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed   # <-- You forgot this line!
+
+    def bark(self):
+        return "Woof!"
+
+    def sit(self):
+        return f"{self.name} is sitting"
